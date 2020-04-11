08f036909985103f3a88e6ea64dee5b18ce45293
Dmitree-Max <d.s.maximenko@gmail.com>
2020-04-11 20:23:04 +0300

    now chat is working

--- js/index.js
+++ js/index.js
@@ -5,20 +5,64 @@ var user = 2;
 var user_name = "Spider Man";
 var last_syn = "";
 
+LoadMessages();
+setTimeout(RenovateMessages, 1000, true);
+
 function LoadMessages() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        //document.getElementById("msgs").innerHTML = this.responseText;
-	MakeMessage("Borisov Kolya", "Who are you?", "16:00:00 AM", "pics/Zelen.jpg");
+		arr = JSON.parse(this.responseText);
+		ShowMessages(arr);
+		SetLastSynTime(arr[0]);
 	}
   };
-  xhttp.open("GET", "php/load_messages.php?ch="+chanel, true);
+  xhttp.open("GET", "php/load_messages.php?ch="+chanel+"&syn="+last_syn, true);
   xhttp.send();
+
+}
+
+function RenovateMessages(renovate_again) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+		//window.alert();
+		arr = JSON.parse(this.responseText);
+		ShowMessages(arr);
+		if (arr.length > 0)
+		{
+			SetLastSynTime(arr[0]);
+		}
+		if (renovate_again)
+		{
+			setTimeout(RenovateMessages, 1000, true);
+		}
+	}
+  };
+  xhttp.open("GET", "php/load_messages.php?ch="+chanel+"&syn="+last_syn, true);
+  xhttp.send();
+
+}
+
+
+function ShowMessages(arr){
+	for (let i = arr.length - 1; i > -1; i--) {
+        	MakeMessage(arr[i].sendername, arr[i].text, arr[i].created, arr[i].content); 
+        }
+}
+
+
+function SetLastSynTime(mes){
+	last_syn = mes.created;
+	//window.alert(last_syn);
 }
 
 function MakeMessage(sender, text, date, pict_src)
 {
+   if (pict_src == null)
+  { 
+	pict_src = "pics/noavatar.jpg";
+  }
    var sender = $('<div/>')
     .addClass("username")
       .text(sender).css("color", "lightblue");
@@ -32,16 +76,22 @@ function MakeMessage(sender, text, date, pict_src)
    var result = $('<div/>')
     .addClass("msg-back").append(msg);
    $('#all-messages').append(result);
-
+  var objDiv = document.getElementById("all-messages");
+  objDiv.scrollTop = objDiv.scrollHeight;
 }
 
 
 function SendMessage() {
   var str = document.getElementById("write-form").innerHTML;
+  if (str=="")
+  {
+	return;
+  }
+  document.getElementById("write-form").innerHTML = "";
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      LoadMessages();
+         RenovateMessages(false);
     }
   };
   xhttp.open("GET", "php/send_message.php?mes="+str+"&ch="+chanel+"&user="+user+"&username="+user_name, true);
--- php/load_messages.php
+++ php/load_messages.php
@@ -1,5 +1,37 @@
-<?php
 
-echo "OK";
+<?php
+include_once("DatabaseConnection.php");
+
+$chanel = $_GET['ch'];
+$syn = $_GET['syn'];
+$pdo = new DatabaseConnection();
+$conn = $pdo->connection();
+
+if ($syn == "")
+{
+$query = "SELECT senderid, sendername, text, created, content FROM messages where chanel = $chanel order by created desc limit 50";
+}
+else
+{
+$query = "SELECT senderid, sendername, text, created, content FROM messages where chanel = $chanel and created > '$syn' order by created desc limit 50";
+}
+$result = $conn->query($query);
+
+if (!$result) {
+    $message  = 'Неверный запрос: ' . "\n";
+    $message .= 'Запрос целиком: ' . $query;
+    die($message);
+}
+
+$myArray = array();
+
+while($row = $result->fetch_assoc()) {
+        $myArray[] = $row;
+    }
+
+echo json_encode($myArray)
+
 
 ?>
+
+
